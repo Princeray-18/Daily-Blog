7132ae20e944d52d94e8d25aee68e0ac5ddc630b
Princeray-18 <rayp11348@gmail.com>
2025-09-16 01:37:20 +0530

    Today Create  Searchbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,15 +5,43 @@ const Header = () => {
   return (
     <div className='mx-8 sm:mx-24 relative'>
       <div className='text-center mt-20 mb-8'>
-  <div className='inline-flex itmes-center justfy-center gap-4 px-6 py-1.5 mb-4 border-primary/40 bg-primary/10 rounded-full 
+  <div className='inline-flex items-center justify-center gap-4 px-6 py-1.5 mb-4 border-primary/40 bg-primary/10 rounded-full 
   text-sm text-primary'>
-    <p>new: Ai feature integrated</p>
-    <img src={assets.star_icon} className='w-2.5' alt=""/>
-
-  </div>
+     <p>New: Ai feature integrated</p>
+          <img 
+            src={assets.gradientBackground} 
+            className='absolute -top-[50px] -z-10 opacity-50' 
+            alt="" 
+          />
+        </div>
+        <h1 className='text-3xl sm:text-6xl font-semibold sm:leading-[4rem] text-gray-700'>
+          Your Own <span className='text-primary'>Blogging</span> <br /> Platform.
+        </h1>
       </div>
-      <img src ={assets.gradientBackground} alt="" className='w-full h-80 object-cover rounded-lg'/> 
 
+      <p className='mx-6sm:mx-8 max-w-2xl m-auto max-sm:text-xs text-gray-500'> This is your space to think out loud, to share what matters, and to write without filters, whether it's one word or a thousand your story starts right here.</p>
+
+      <form className='flex justify-between max-w-lg max-sm:scale-75 mx-auto
+      border border-gray-300 bg-white rounded overflow-hidden mt-6'>
+        <input 
+          type="text" 
+          placeholder='Search for blogs' 
+          required 
+          className='flex-1 px-4 py-2 outline-none'
+        />
+        <button 
+          type="submit" 
+          className='bg-primary text-white px-8 py-2 m-1.5
+          rounded hover:scale-105 transition-all cursor-pointer'>
+          Search
+        </button>
+      </form>
+
+      <img 
+        src={assets.gradientBackground} 
+        alt="" 
+        className='w-full h-80 object-cover rounded-lg mt-8' 
+      /> 
     </div>
   )
 }
